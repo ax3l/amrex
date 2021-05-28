ae7be05662cfdb51a27a105e574f01abd65f3767
Axel Huebl <axel.huebl@plasma.ninja>
2021-05-27 19:10:07 -0700

    CI: NVHPC
    Add a CI entry that compiles the host C++ side with nvc++ and
    the device side with nvcc.
    
    This seems to be the default Cray-ed up `PrgEnv-nvidia` that we
    receive on Perlmutter.

---
+++ .github/workflows/dependencies/dependencies_nvhpc21-3.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Copyright 2021 Axel Huebl
+#
+# License: BSD-3-Clause-LBNL
+
+set -eu -o pipefail
+
+sudo apt-get -qqq update
+sudo apt-get install -y \
+    build-essential     \
+    ca-certificates     \
+    cmake               \
+    environment-modules \
+    g++                 \
+    gfortran            \
+    gnupg               \
+    libopenmpi-dev      \
+    openmpi-bin         \
+    pkg-config          \
+    wget
+
+wget -q https://developer.download.nvidia.com/hpc-sdk/21.3/nvhpc-21-3_21.3_amd64.deb \
+        https://developer.download.nvidia.com/hpc-sdk/21.3/nvhpc-2021_21.3_amd64.deb
+sudo apt-get update
+sudo apt-get install -y ./nvhpc-21-3_21.3_amd64.deb ./nvhpc-2021_21.3_amd64.deb
+rm -rf ./nvhpc-21-3_21.3_amd64.deb ./nvhpc-2021_21.3_amd64.deb
+
+# things should reside in /opt/nvidia/hpc_sdk now
+
+# activation via:
+#   source /etc/profile.d/modules.sh
+#   module load /opt/nvidia/hpc_sdk/modulefiles/nvhpc/20.7
